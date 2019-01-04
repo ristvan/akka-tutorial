57d3a425d69bc53f000a18a76f9a932164f99404
ristvan <reistvan@gmail.com>
2019-01-04 15:27:05 +0100

    Add test to check increasing number generation
    One of the cases case should check what happens when a
    StartProduction event is sent to when it is already
    ongoing.
    
    Signed-off-by: ristvan <reistvan@gmail.com>

--- src/test/scala/com/istvan/rejto/tutorial/akka/NumberProducerTest.scala
+++ src/test/scala/com/istvan/rejto/tutorial/akka/NumberProducerTest.scala
@@ -45,9 +45,9 @@ class NumberProducerTest
   }
 
   "NumberProducer actor" should {
-    "produce number only it is requested" in {
-      val numberProducer = system.actorOf(NumberProducer.props(this.testActor))
+    val numberProducer = system.actorOf(NumberProducer.props(this.testActor))
 
+    "produce number only between start and stop" in {
       numberProducer ! NumberProducer.StartProduction
       expectMsg(1)
       expectMsg(2)
@@ -55,7 +55,33 @@ class NumberProducerTest
       numberProducer ! NumberProducer.StopProduction
       expectNoMessage(FiniteDuration(1, "second"))
     }
+
+    "during number production StartProduction will not do anything" in {
+      numberProducer ! NumberProducer.StartProduction
+      expectMsg(4)
+      expectMsg(5)
+      numberProducer ! NumberProducer.StartProduction
+      expectMsg(6)
+      expectMsg(7)
+      numberProducer ! NumberProducer.StopProduction
+      expectNoMessage(FiniteDuration(1, "second"))
+    }
   }
 
+  "The numbers during production" should {
+    "be increasing by 1" in {
+      val numberProducer = system.actorOf(NumberProducer.props(this.testActor))
+      numberProducer ! NumberProducer.StartProduction
+      expectMsg(1)
+      expectMsg(2)
+      expectMsg(3)
+      expectMsg(4)
+      expectMsg(5)
+      expectMsg(6)
+      expectMsg(7)
+      numberProducer ! NumberProducer.StopProduction
+      expectNoMessage(FiniteDuration(1, "second"))
 
+    }
+  }
 }
